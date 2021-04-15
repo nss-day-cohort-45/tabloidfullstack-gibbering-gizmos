f7d54d0222bb63c0c8eff21834d838437c73c862
Christina Ashworth <christina.ashworth@ymail.com>
2021-04-15 15:11:35 -0500

    skeleton code for posts

---
+++ Tabloid/Repositories/PostRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Tabloid.Models;
+using Tabloid.Utils;
+using System.Collections.Generic;
+
+namespace Tabloid.Repositories
+{
+    public class PostRepository : BaseRepository, IPostRepository
+    {
+        public PostRepository(IConfiguration configuration) : base(configuration) { }
+        public List<Post> GetAllPosts()
+        { }
+        public List<Post> GetCurrentUserPosts(int userProfileId)
+        { }
+        public Post GetPostById(int id)
+        { }
+        public void AddPost(Post post)
+        { }
+        public void UpdatePost(Post post)
+        { }
+        public void DeletePost(int id)
+        { }
+    }
+}
