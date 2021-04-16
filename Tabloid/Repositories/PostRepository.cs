3b4694efb58644da29eb9a3fba20319474e0921f
Ryan Youngblood <rryb77@gmail.com>
2021-04-16 09:00:29 -0500

    user posts are sorted by most recent to oldest

--- Tabloid/Repositories/PostRepository.cs
+++ Tabloid/Repositories/PostRepository.cs
@@ -20,19 +20,24 @@ namespace Tabloid.Repositories
                 {
                     cmd.CommandText = @"
                     SELECT p.Id, p.Title, p.Content, 
-                              p.ImageLocation AS HeaderImage,
-                              p.CreateDateTime, p.PublishDateTime, p.IsApproved,
-                              p.CategoryId, p.UserProfileId,
-                              c.[Name] AS CategoryName,
-                              u.FirstName, u.LastName, u.DisplayName, 
-                              u.Email, u.CreateDateTime, u.ImageLocation AS AvatarImage,
-                              u.UserTypeId, 
-                              ut.[Name] AS UserTypeName
-                         FROM Post p
-                              LEFT JOIN Category c ON p.CategoryId = c.id
-                              LEFT JOIN UserProfile u ON p.UserProfileId = u.id
-                              LEFT JOIN UserType ut ON u.UserTypeId = ut.id
-                        WHERE p.UserProfileId = @id
+                                p.ImageLocation AS HeaderImage,
+                                p.CreateDateTime, p.PublishDateTime, p.IsApproved,
+                                p.CategoryId, p.UserProfileId,
+
+                                c.[Name] AS CategoryName,
+
+                                u.FirstName, u.LastName, u.DisplayName, 
+                                u.Email, u.CreateDateTime, u.ImageLocation AS AvatarImage,
+                                u.UserTypeId, 
+
+                                ut.[Name] AS UserTypeName
+
+                            FROM Post p
+                                LEFT JOIN Category c ON p.CategoryId = c.id
+                                LEFT JOIN UserProfile u ON p.UserProfileId = u.id
+                                LEFT JOIN UserType ut ON u.UserTypeId = ut.id
+                            WHERE p.UserProfileId = @id
+                            ORDER BY p.CreateDateTime DESC
                     ";
 
                     cmd.Parameters.AddWithValue("id", userProfileId);
