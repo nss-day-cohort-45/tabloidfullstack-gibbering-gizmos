f30fb01e724071d6f97346372d91ecd725a24a02
Ryan Youngblood <rryb77@gmail.com>
2021-04-19 11:24:17 -0500

    API for delete category was created/tested and go desired results!

--- Tabloid/Repositories/CategoryRepository.cs
+++ Tabloid/Repositories/CategoryRepository.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Tabloid.Models;
+using Tabloid.Utils;
+using Microsoft.Data.SqlClient;
 
 namespace Tabloid.Repositories
 {
@@ -18,7 +20,44 @@ namespace Tabloid.Repositories
 
         public void DeleteCategory(int id)
         {
-            
+            var categories = GetAllCategories();
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    UPDATE Post
+                        SET CategoryId = @CategoryId
+                    WHERE CategoryId = @id
+                    ";
+
+                    DbUtils.AddParameter(cmd, "@id", id);
+                    if (id != categories[0].Id)
+                    {
+                        DbUtils.AddParameter(cmd, "@CategoryId", categories[0].Id);
+                    }
+                    else
+                    {
+                        DbUtils.AddParameter(cmd, "@CategoryId", categories[1].Id);
+                    }
+                    
+
+                    cmd.ExecuteNonQuery();
+                }
+                
+                using(var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    DELETE From Category
+                    WHERE Id = @id
+                    ";
+
+                    DbUtils.AddParameter(cmd, "@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public List<Category> GetAllCategories()
