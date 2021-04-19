e190dfe91a22fd4065145452f90ac2a85d6862d9
cslunetta <cslunetta@outlook.com>
2021-04-19 15:49:21 -0500

    Added Tag Controller and Tag Repository connections

--- Tabloid/Controllers/TagController.cs
+++ Tabloid/Controllers/TagController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tabloid.Models;
 
 namespace Tabloid.Controllers
 {
@@ -11,5 +12,11 @@ namespace Tabloid.Controllers
     [ApiController]
     public class TagController : ControllerBase
     {
+        [HttpPost]
+        public IActionResult Post(Tag tag)
+        {
+            _tagRepository.AddTag(tag);
+            return CreatedAtAction("Get", new { id = tag.Id }, tag);
+        }
     }
 }
