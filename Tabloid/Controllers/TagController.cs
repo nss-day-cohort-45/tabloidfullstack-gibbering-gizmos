e7c594b8d62d921a6184807078b2e1ba1bdb2ba9
Ryan Youngblood <rryb77@gmail.com>
2021-04-20 10:59:25 -0500

    small fix in tagcontroller

--- Tabloid/Controllers/TagController.cs
+++ Tabloid/Controllers/TagController.cs
@@ -31,5 +31,25 @@ namespace Tabloid.Controllers
             _tagRepository.AddTag(tag);
             return CreatedAtAction("Get", new { id = tag.Id }, tag);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var tag = _tagRepository.GetTagById(id);
+            if (tag == null)
+            {
+                return null;
+            }
+
+            return Ok(tag);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _tagRepository.DeleteTag(id);
+            return NoContent();
+        }
     }
 }
