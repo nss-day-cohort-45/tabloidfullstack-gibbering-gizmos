db957ff7ccbf9a5cf2ebcc7037db60c3f2d273f8
Christina Ashworth <christina.ashworth@ymail.com>
2021-04-21 15:58:56 -0500

    working on back end of comment and add comments

---
+++ Tabloid/Repositories/CommentRepository.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid.Models;
+
+namespace Tabloid.Repositories
+{
+    public class CommentRepository : BaseRepository, ICommentRepository
+    {
+        public CommentRepository(IConfiguration config) : base(config) { }
+        public List<Comment> GetAllComments()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        SELECT c.Id, c.PostId,                               c.UserProfileId, c.Subject, c.Content, c.CreateDateTime, p.Title AS PostTitle, u.DisplayName
+                                        FROM Comment c
+                                            LEFT JOIN Post p ON c.PostId = p.Id
+                                            LEFT JOIN UserProfile u ON p.UserProfileId = u.Id
+                                        ORDER BY c.CreateDateTime DESC";
+                    var reader = cmd.ExecuteReader();
+
+                    var comments = new List<Comment>();
+
+                    while (reader.Read())
+                    {
+                        Comment comment = new Comment
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PostId = reader.GetInt32(reader.GetOrdinal("PostId")),
+                            UserProfileId = reader.GetInt32(reader.GetOrdinal("UserProfileId")),
+                            Subject = reader.GetString(reader.GetOrdinal("Subject")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                        };
+
+                        comment.Post = new Post()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("PostId")),
+                            Title = reader.GetString(reader.GetOrdinal("PostTitle"))
+                        };
+
+                        comment.UserProfile = new UserProfile()
+                        {
+                            DisplayName = reader.GetString(reader.GetOrdinal("DisplayName"))
+                        };
+                        comments.Add(comment);
+                    }
+
+                    reader.Close();
+
+                    return comments;
+                }
+            }
+        }
+
+        public void AddComment(Comment comment)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        INSERT INTO Comment (PostId, UserProfileId, Subject, Content, CreateDateTime)
+                                        OUTPUT INSERTED.ID
+                                        VALUES (@PostId, @UserProfileId, @Subject, @Content, @CreateDateTime";
+
+                    cmd.Parameters.AddWithValue("@PostId", comment.PostId);
+                    cmd.Parameters.AddWithValue("@UserProfileId", comment.UserProfileId);
+                    cmd.Parameters.AddWithValue("@Subject", comment.Subject);
+                    cmd.Parameters.AddWithValue("@Content", comment.Content);
+                    cmd.Parameters.AddWithValue("@CreateDateTime", comment.CreateDateTime);
+
+                    comment.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+    }
+}
